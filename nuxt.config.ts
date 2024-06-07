e5ecc3d2cf0960887f9e0d40d5b1f8e51a78b2cf
badrnyali <badr.nya@gmail.com>
2024-06-07 12:48:08 +0200

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          "Space+Grotesk": true,
-        },
+  modules: [[
+    "@nuxtjs/google-fonts",
+    {
+      families: {
+        "Space+Grotesk": true,
       },
-    ]
-  ],
+    },
+  ], "@nuxtjs/sitemap"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
