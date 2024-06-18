d82a9c07af18d4fb9e893b34fea1175d92b1b17d
badrnyali <badr.nya@gmail.com>
2024-06-18 15:36:25 +0200

    fix

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,37 +1,82 @@
 <template>
-      <div class="container mx-auto my-14 px-4 py-8">
-        <div class="bg-white shadow-md rounded-lg p-6">
-            <h1 class="text-3xl font-bold mb-6 text-center">Privacy Policy</h1>
-            <p class="text-gray-600 mb-4">Effective Date: June 18, 2024</p>
+  <div class="container mx-auto my-14 px-4 py-8">
+    <div class="bg-white shadow-md rounded-lg p-6">
+      <h1 class="text-3xl font-bold mb-6 text-center">Privacy Policy</h1>
+      <p class="text-gray-600 mb-4">Effective Date: June 18, 2024</p>
 
-            <h2 class="text-2xl font-semibold mb-4">Introduction</h2>
-            <p class="mb-4">Welcome to Pokémon Name Generator!</p>
-            <p class="mb-6">At Pokémon Name Generator, we prioritize your privacy and data security. This Privacy Policy outlines how we handle and protect your information when you use our Pokémon name generator service.</p>
+      <h2 class="text-2xl font-semibold mb-4">Introduction</h2>
+      <p class="mb-4">Welcome to Pokémon Name Generator!</p>
+      <p class="mb-6">
+        At Pokémon Name Generator, we prioritize your privacy and data security.
+        This Privacy Policy outlines how we handle and protect your information
+        when you use our Pokémon name generator service.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Information Collection</h2>
-            <p class="mb-6">We do not collect any personal information or data from our users. When you use our Pokémon name generator, you can rest assured that no identifiable information is collected, stored, or processed.</p>
+      <h2 class="text-2xl font-semibold mb-4">Information Collection</h2>
+      <p class="mb-6">
+        We do not collect any personal information or data from our users. When
+        you use our Pokémon name generator, you can rest assured that no
+        identifiable information is collected, stored, or processed.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Cookies</h2>
-            <p class="mb-6">Our service does not use cookies or any other tracking technologies. We respect your privacy and ensure that your browsing experience remains anonymous and secure.</p>
+      <h2 class="text-2xl font-semibold mb-4">Cookies</h2>
+      <p class="mb-6">
+        Our service does not use cookies or any other tracking technologies. We
+        respect your privacy and ensure that your browsing experience remains
+        anonymous and secure.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Data Security</h2>
-            <p class="mb-6">Even though we do not collect any data, we are committed to maintaining a secure environment for our users. Our website and service are built with security in mind to protect against any potential vulnerabilities.</p>
+      <h2 class="text-2xl font-semibold mb-4">Data Security</h2>
+      <p class="mb-6">
+        Even though we do not collect any data, we are committed to maintaining
+        a secure environment for our users. Our website and service are built
+        with security in mind to protect against any potential vulnerabilities.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Third-Party Links</h2>
-            <p class="mb-6">Our service may contain links to third-party websites. Please be aware that we are not responsible for the privacy practices of these external sites. We encourage you to read the privacy policies of any third-party websites you visit.</p>
+      <h2 class="text-2xl font-semibold mb-4">Third-Party Links</h2>
+      <p class="mb-6">
+        Our service may contain links to third-party websites. Please be aware
+        that we are not responsible for the privacy practices of these external
+        sites. We encourage you to read the privacy policies of any third-party
+        websites you visit.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Changes to This Privacy Policy</h2>
-            <p class="mb-6">We may update this Privacy Policy from time to time. Any changes will be posted on this page, and we will update the effective date at the top of the policy. We encourage you to review this Privacy Policy periodically to stay informed about how we protect your privacy.</p>
+      <h2 class="text-2xl font-semibold mb-4">
+        Changes to This Privacy Policy
+      </h2>
+      <p class="mb-6">
+        We may update this Privacy Policy from time to time. Any changes will be
+        posted on this page, and we will update the effective date at the top of
+        the policy. We encourage you to review this Privacy Policy periodically
+        to stay informed about how we protect your privacy.
+      </p>
 
-            <h2 class="text-2xl font-semibold mb-4">Contact Us</h2>
-            <p class="mb-6">If you have any questions or concerns about our Privacy Policy or our practices, please contact us at kikomeb315@gmail.com .</p>
+      <h2 class="text-2xl font-semibold mb-4">Contact Us</h2>
+      <p class="mb-6">
+        If you have any questions or concerns about our Privacy Policy or our
+        practices, please contact us at kikomeb315@gmail.com .
+      </p>
 
-            <p class="text-center">Thank you for using Pokémon Name Generator!</p>
-        </div>
+      <p class="text-center">Thank you for using Pokémon Name Generator!</p>
     </div>
-  </template>
-  
-  <script setup></script>
-  
-  <style lang="scss" scoped></style>
-  
+  </div>
+</template>
+
+<script setup>
+  useHead({
+  title: "Privacy policy - Pokemon name generator",
+  link: {
+    rel: "canonical",
+    href: "https://pokemonnamegenerator.com/privacy-policy",
+  },
+  meta: [
+    {
+      name: "description",
+      content:
+        "Our privacy policy explains how we collect, use, and protect your personal information on pokemonnamegenerator.com. We take your privacy seriously and use security measures to protect your data.",
+    },
+  ],
+});
+</script>
+
+<style lang="scss" scoped></style>
