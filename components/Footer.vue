be05c45dc6c72b29bce4695c4261ae1d3666eff5
badrnyali <badr.nya@gmail.com>
2024-06-18 17:26:43 +0200

    fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full py-4 px-14 bg-[#3b4cca74]">
     <ul class="flex items-center gap-x-4 justify-center">
       <li class="cursor-pointer underline text-xs">
-        <NuxtLink to="/privacy-policy">Privacy Policy</NuxtLink>
+        <NuxtLink to="/privacy-policy" rel="nofollow">Privacy Policy</NuxtLink>
       </li>
     </ul>
   </footer>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { useFetch } from "nuxt/app";
 
+const submited = ref(false);
 useHead({
   title: "Pokemon name generator - Generate random names",
   link: {
@@ -20,12 +21,16 @@ useHead({
 const pokemonName = ref("");
 
 const submit = async () => {
+  submited.value = true;
   const { data, error } = await useFetch("/api/pokemon");
   if (!error.value) {
-    pokemonName.value = data.value.pokemonName;
+    pokemonName.value = data.value;
   } else {
     console.error(error.value);
   }
+  setTimeout(() => {
+    submited.value = false;
+  }, 500);
 };
 function copyName() {
   navigator.clipboard.writeText(pokemonName.value);
@@ -42,31 +47,92 @@ function copyName() {
         Welcome to Pokemon Name Generator, your ultimate tool for generating
         random Pokemon names! Our service helps you find unique and fun names
         for your Pokemon adventures. Simply click the button and get a random
-        Pokemon name instantly.
+        name instantly.
       </p>
       <button
-        class="mt-10 bg-[#ffde00] text-[#000] font-semibold text-xl px-4 py-2 rounded-md cursor-pointer"
+        :class="{ 'pointer-events-none': submited }"
+        class="mt-10 bg-[#ffdd00ad] hover:bg-[#ffde00] text-[#000] font-semibold text-xl px-4 py-2 rounded-md cursor-pointer"
         @click="submit"
       >
         Random Name
       </button>
-      <h3
-        v-if="pokemonName.length"
-        class="relative text-4xl font-bold tracking-wider my-10"
+      <Loading v-if="submited" />
+      <div
+        class="w-full flex flex-col items-center gap-y-4"
+        v-if="!submited && pokemonName.name"
       >
-        {{ pokemonName }}
-        <div
-          class="absolute right-[-24px] top-1 cursor-pointer"
-          @click="copyName"
-        >
-          <icon
-            class="absolute right-[-4px] top-2 h-4 w-4 rounded-sm border border-solid bg-white border-[#000]"
-          ></icon>
-          <icon
-            class="absolute right-[-10px] top-3 h-4 w-4 rounded-sm border border-solid bg-white border-[#000]"
-          ></icon>
-        </div>
-      </h3>
+        <h3 class="relative text-4xl font-bold tracking-wider mt-10 mb-4">
+          {{ pokemonName.name }}
+          <div
+            class="absolute right-[-24px] top-1 cursor-pointer"
+            @click="copyName"
+          >
+            <icon
+              class="absolute right-[-4px] top-2 h-4 w-4 rounded-sm border border-solid bg-white border-[#000]"
+            ></icon>
+            <icon
+              class="absolute right-[-10px] top-3 h-4 w-4 rounded-sm border border-solid bg-white border-[#000]"
+            ></icon>
+          </div>
+        </h3>
+        <p class="text-xl tracking-wide">
+          <span class="font-bold">{{ pokemonName.name }} is</span>
+          {{ pokemonName.description.toLowerCase() }}
+        </p>
+        <p class="text-xl tracking-wide">
+          <span class="font-bold"
+            >Special skills of {{ pokemonName.name }} are
+          </span>
+          {{ pokemonName.skills.join(" , ") }}.
+        </p>
+        <p class="flex items-center justify-center text-xl tracking-wide">
+          Find more about this <img class="mx-2" src="/public/pokemon.svg" />
+          <span class="underline">
+            <NuxtLink
+              :to="`https://wiki.pokemoncentral.it/${pokemonName.name}`"
+              rel="noopener"
+              target="_blank"
+            >
+              here.</NuxtLink
+            ></span
+          >
+        </p>
+      </div>
+      <div class="w-full flex flex-col text-left items-center justify-center">
+        <h2 class="text-4xl mt-14 mb-4">
+          The History of Pokemon: A Timeless Adventure
+        </h2>
+        <p class="text-lg mb-2 tracking-wide">
+          Created by Satoshi Tajiri and Ken Sugimori in 1996, Pokemon became a
+          cultural phenomenon. Pokemon was developed in Japan, initially as a
+          video game. It has since then captured the attention of millions of people
+          worldwide, creating a multimedia realm of animated TV shows, trading
+          card games, movies, merchandise, and more
+        </p>
+        <figure class="my-4 mb-8">
+          <img
+            src="/public/founders.jpg"
+            class="min-h-[300px] object-contain"
+            alt="Founders of pokemon"
+            title="Founders of pokemon"
+          />
+          <figcaption class="text-xs">
+            Founders of pokemon
+          </figcaption>
+        </figure>
+        <p class="text-lg mb-2 tracking-wide">
+          Pokemon’s historical impact lies in the gameplay and social aspects it
+          showed. Known as trainers, players go on expeditions to capture,
+          train, and fight creatures, each with unique abilities and skills.
+          This concept was revolutionary at the time, offering a mixture of
+          style, adventure and competition that appealed to children as well as
+          adults. The Pokemon slogan,
+          <span class="font-semibold italic"
+            >"Gotta Catch 'Em All!",</span
+          >
+           resonated deeply with fans and emphasized the game's central theme of exploration and discovery.
+        </p>
+      </div>
     </article>
   </main>
 </template>
